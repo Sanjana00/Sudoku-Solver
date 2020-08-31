c00e1ee3f1e129c5f137e4682f3fb1e0919ca1ce
Sanjana Chakravarty <sanjana.chakravarty@gmail.com>
2020-08-31 12:39:26 +0530

    Added docstrings to functions

--- sudoku.py
+++ sudoku.py
@@ -21,22 +21,28 @@ class Sudoku:
         self.flag = True
 
     def find_row(self, pos):
+        ''' Given a position on the board, returns list of the remaining positions in the same row '''
         return [pos[0] + y for y in NUM if y != pos[1]]
 
     def find_col(self, pos):
+        ''' Given a position on the board, returns list of the remaining positions in the same column '''
         return [x + pos[1] for x in ALPHA if x != pos[0]]
 
     def find_square(self, pos):
+        ''' Given a position on the board, returns list of the remaining positions in the same square '''
         for square in SQUARES:
             if pos in square:
                 return square
 
     def possible_move(self, pos):
+        ''' Given a position on the board, returns list of possible numbers that can fill that position.
+        A number may fill the position if it is not present in any row, column or square which the position is a part of. '''
         related = set(self.find_row(pos) + self.find_col(pos) + self.find_square(pos))
         contents = [self.board[x] for x in related]
         return [x for x in range(1, 10) if x not in contents]
 
     def naked_singles(self):
+        ''' Updates the possible moves for each position and assigns value to position if there is no other valid move possible '''
         for pos in self.possible.keys():
             if self.board[pos] == 0:
                 self.possible[pos] = self.possible_move(pos)
@@ -45,17 +51,20 @@ class Sudoku:
                     self.flag = True
 
     def solve_naked_singles(self):
+        ''' Tries to solve for naked singles until there are no more naked singles left for the given state of the board '''
         while self.flag:
             self.flag = False
             self.naked_singles()
         self.flag = True
 
     def solve_hidden_singles(self):
+        ''' Identifies hidden singles in each row, column and square of board '''
         self.hidden_singles(ROWS)
         self.hidden_singles(COLS)
         self.hidden_singles(SQUARES)
 
     def hidden_singles(self, GROUPS):
+        ''' Identifies hidden singles in group (could be rows, columns or squares) and eliminates possibilities based on them '''
         for group in GROUPS:
             possibles = [self.possible[pos] for pos in group]
             for i in range(1, 10):
@@ -66,6 +75,7 @@ class Sudoku:
         self.solve_naked_singles()
 
     def solve_naked_pairs(self):
+        ''' Identifies naked pairs/triples in rows, columns and squares '''
         self.naked_pairs(ROWS)
         self.naked_pairs(COLS)
         self.naked_pairs(SQUARES)
@@ -74,6 +84,7 @@ class Sudoku:
         self.naked_pairs(SQUARES, 3)
 
     def naked_pairs(self, GROUPS, key = 2):
+        ''' Identifies naked pairs (or triples or quads) in group (rows, columns or squares) and eliminates possibilities based on them '''
         for group in GROUPS:
             possibles = {pos : self.possible[pos] for pos in group if len(self.possible[pos]) == key}
             hist = []
@@ -97,6 +108,7 @@ class Sudoku:
         self.solve_hidden_singles()
 
     def solve_hidden_pairs(self):
+        ''' Identifies hidden pairs/triples in rows, columns and squares '''
         self.hidden_pairs(ROWS)
         self.hidden_pairs(COLS)
         self.hidden_pairs(SQUARES)
@@ -105,6 +117,7 @@ class Sudoku:
         self.hidden_pairs(SQUARES, 3)
 
     def hidden_pairs(self, GROUPS, key = 2):
+        ''' Identifies hidden pairs (or triples or quads) in groups (rows, columns or squares) and eliminates possibilities based on them '''
         for group in GROUPS:
             hist = []
             possibles = [[]] + [[pos for pos in group if i in self.possible[pos]] for i in range(1, 10)]
@@ -129,21 +142,25 @@ class Sudoku:
         self.solve_naked_pairs()
 
     def solve_naked_quads(self):
+        ''' Identifies naked quads in rows, columns and squares '''
         self.naked_pairs(ROWS, 3)
         self.naked_pairs(COLS, 3)
         self.naked_pairs(SQUARES, 3)
         self.solve_hidden_pairs()
 
     def solve_hidden_quads(self):
+        ''' Identifies hidden quads in rows, columns and squares '''
         self.hidden_pairs(ROWS, 4)
         self.hidden_pairs(COLS, 4)
         self.hidden_pairs(SQUARES, 4)
         self.solve_naked_quads()
 
     def is_solved(self):
+        ''' Returns True if all squares are full, else False '''
         return list(self.board.values()).count(0) == 0
 
     def solve(self):
+        ''' Solves an incomplete sudoku grid '''
         self.solve_naked_singles()
         if not self.is_solved():
             self.solve_hidden_singles()
@@ -160,6 +177,7 @@ class Sudoku:
             self.solve_backtrack()
     
     def solve_backtrack(self):
+        ''' Solves a sudoku grid recursively by using trial and error '''
         for pos in BOARD:
             if self.board[pos] == 0:
                 for i in range(1, 10):
@@ -173,15 +191,18 @@ class Sudoku:
         sys.exit(0)
 
     def display(self):
+        ''' Displays the sudoku board '''
         print()
         for i in range(0, 73, 9):
             print(*list(self.board.values())[i : i + 9])
         print()
 
     def is_valid(self):
+        ''' Returns True if current sudoku board is a correct solution, else False '''
         return self.group_validate(ROWS) and self.group_validate(COLS) and self.group_validate(SQUARES)
 
     def validate(self):
+        ''' Prints message for verdict of solution '''
         print("Verdict: \n")
         if self.is_valid():
             print("Answer seems totally correct, yay!")
@@ -190,6 +211,7 @@ class Sudoku:
         print()
 
     def group_validate(self, GROUPS):
+        ''' Checks if all numbers from 1 - 9 are present exactly once in the groups (rows, columns or squares) '''
         for group in GROUPS:
             contents = [self.board[pos] for pos in group]
             if not all(contents.count(i) == 1 for i in range(1, 10)):
